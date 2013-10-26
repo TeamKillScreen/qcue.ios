37f04b5c230daf1b649d31beaf96a1f275698db8
Alan Gorton <dev.code1up@googlemail.com>
2013-10-26 22:01:51 +0100

    wip

---
+++ Qcue/QueuesViewController.h
@@ -0,0 +1,13 @@
+//
+//  QueuesViewController.h
+//  Qcue
+//
+//  Created by Alan Gorton on 26/10/2013.
+//  Copyright (c) 2013 TeamKillScreen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QueuesViewController : UITableViewController
+
+@end
