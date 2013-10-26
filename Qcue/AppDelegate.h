7414022b888507d3ffba710fc13512564e7dcbb3
Alan Gorton <dev.code1up@googlemail.com>
2013-10-26 14:36:17 +0100

    Initial Commit

---
+++ Qcue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Qcue
+//
+//  Created by Alan Gorton on 26/10/2013.
+//  Copyright (c) 2013 TeamKillScreen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
